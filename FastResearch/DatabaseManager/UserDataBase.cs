b6e3a1d7fe144ceb73fed223d327dfb9644d8c6f
ch4osmy7h <ch4osmy7h@gmail.com>
2019-03-24 00:04:22 +0800

    database first create

---
+++ FastResearch/DatabaseManager/UserDataBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+
+
+/// <summary>
+/// Sqlite版本一定要是1.1.1以下
+/// </summary>
+namespace FastResearch.DatabaseManager
+{
+    public static class UserDataBase
+    {
+        public static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+                new SqliteConnection("Filename=user.db"))
+            {
+                db.Open();
+                String tableCommand = "CREATE TABLE IF NOT " +
+            "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+            "Text_Entry NVARCHAR(2048) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                createTable.ExecuteReader();
+            }
+        }
+
+    }
+}
--- FastResearch/MainPage.xaml.cs
+++ FastResearch/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Media.Animation;
 using System.Linq;
+using FastResearch.DatabaseManager;
 
 namespace FastResearch
 {
