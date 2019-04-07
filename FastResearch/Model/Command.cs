d331d0b4ecea0d10df891d169932c8fe620aa6e5
thankod <thankoder@gmail.com>
2019-04-07 18:59:33 +0800

    try

---
+++ FastResearch/Model/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastResearch.Model
+{
+    public class Command
+    {
+        public string name;
+        public string executable;
+        public string file;
+        public List<Tuple<string, string>> args;
+        public string description;
+    }
+}
